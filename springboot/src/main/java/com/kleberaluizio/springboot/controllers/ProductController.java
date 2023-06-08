06c02a547a59e0df835656495fa8eb6302a337ac
kleberaluizio <kleberaluizio@gmail.com>
2023-06-08 20:41:06 -0300

    Understanding Beans annotations

---
+++ springboot/src/main/java/com/kleberaluizio/springboot/controllers/ProductController.java
@@ -0,0 +1,31 @@
+package com.kleberaluizio.springboot.controllers;
+
+import com.kleberaluizio.springboot.dtos.ProductRecordDto;
+import com.kleberaluizio.springboot.models.ProductModel;
+import com.kleberaluizio.springboot.repositories.ProductRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // Bean do Spring que tem API Rest
+@RequestMapping
+public class ProductController {
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @PostMapping("/products")
+    public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDto productRecordDto){
+        var productModel = new ProductModel();
+        BeanUtils.copyProperties(productRecordDto, productModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
+    }
+
+
+}
